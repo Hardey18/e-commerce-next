cc49da90753e5386e085f2bf386a9ef6ebc67a53
Nurudeen Adewale <nurudeenadeolaa@gmail.com>
2022-01-21 23:16:47 +0100

    Admin dashboard page

--- components/Layout.js
+++ components/Layout.js
@@ -122,6 +122,15 @@ export default function Layout({ title, description, children }) {
                     >
                       Order Hisotry
                     </MenuItem>
+                    {userInfo.isAdmin && (
+                      <MenuItem
+                        onClick={(e) =>
+                          loginMenuCloseHandler(e, '/admin/dashboard')
+                        }
+                      >
+                        Admin Dashboard
+                      </MenuItem>
+                    )}
                     <MenuItem onClick={logoutClickHandler}>Logout</MenuItem>
                   </Menu>
                 </>
