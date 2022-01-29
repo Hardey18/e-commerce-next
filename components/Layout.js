2bf57f9a3932255cdddc5b8f28697d5f03a2f764
Nurudeen Adewale <nurudeenadeolaa@gmail.com>
2022-01-29 20:13:16 +0100

    Solved the userInfo reload state issue

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,11 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { AppBar, Container, Link, Toolbar, Typography, Switch, Badge, Button, Menu, MenuItem, Box,
-  IconButton,
-  Drawer,
-  List,
-  ListItem,
-  Divider,
-  ListItemText, InputBase, } from '@mui/material';
-import Head from 'next/head'
+// import React, { useContext } from 'react';
+import Head from 'next/head';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
 import { createTheme } from '@mui/material/styles';
 
 import useMediaQuery from '@mui/material/useMediaQuery';
+import React, { useContext, useEffect, useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import CancelIcon from '@mui/icons-material/Cancel';
 import SearchIcon from '@mui/icons-material/Search';
@@ -21,38 +15,65 @@ import Cookies from 'js-cookie';
 import { useSnackbar } from 'notistack';
 import axios from 'axios';
 import { useRouter } from 'next/router';
-import NextLink from 'next/link'
+import NextLink from 'next/link';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Link,
+  Switch,
+  Badge,
+  Button,
+  Menu,
+  MenuItem,
+  Box,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  Divider,
+  ListItemText,
+  InputBase,
+} from '@mui/material';
 import { Store } from '../utils/Store';
 
 export default function Layout({ title, description, children }) {
-    const { state, dispatch } = useContext(Store);
-    const { darkMode, cart, userInfo } = state;
-    const theme = createTheme({
-        typography: {
-            h1: {
-              fontSize: '1.6rem',
-              fontWeight: 400,
-              margin: '1rem 0',
-            },
-            h2: {
-              fontSize: '1.4rem',
-              fontWeight: 400,
-              margin: '1rem 0',
-            },
-          },
-          palette: {
-            mode: darkMode ? 'dark' : 'light',
-            primary: {
-              main: '#f0c000',
-            },
-            secondary: {
-              main: '#208080',
-            },
-          },
-        });
-        const router = useRouter();
+  const { state, dispatch } = useContext(Store);
+  const { darkMode, cart, userInfo } = state;
+  const theme = createTheme({
+    components: {
+      MuiLink: {
+        defaultProps: {
+          underline: 'hover',
+        },
+      },
+    },
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
+  const router = useRouter();
 
-    const [sidbarVisible, setSidebarVisible] = useState(false);
+  const [sidbarVisible, setSidebarVisible] = useState(false);
   const sidebarOpenHandler = () => {
     setSidebarVisible(true);
   };
@@ -84,12 +105,13 @@ export default function Layout({ title, description, children }) {
   useEffect(() => {
     fetchCategories();
   }, []);
-    const darkModeChangeHandler = () => {
-        dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
-        const newDarkMode = !darkMode;
-        Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
-      };
-      const [anchorEl, setAnchorEl] = useState(null);
+
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode;
+    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+  };
+  const [anchorEl, setAnchorEl] = useState(null);
   const loginClickHandler = (e) => {
     setAnchorEl(e.currentTarget);
   };
@@ -109,13 +131,13 @@ export default function Layout({ title, description, children }) {
     router.push('/');
   };
   const isDesktop = useMediaQuery('(min-width:600px)');
-    return (
-        <>
-            <Head>
-            <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
+  return (
+    <>
+      <Head>
+        <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
         {description && <meta name="description" content={description}></meta>}
-            </Head>
-              <ThemeProvider theme={theme}>
+      </Head>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
         <AppBar position="static" sx={classes.appbar}>
           <Toolbar sx={classes.toolbar}>
@@ -130,7 +152,7 @@ export default function Layout({ title, description, children }) {
               </IconButton>
               <NextLink href="/" passHref>
                 <Link>
-                <Typography sx={classes.brand}>amazona</Typography>
+                  <Typography sx={classes.brand}>amazona</Typography>
                 </Link>
               </NextLink>
             </Box>
@@ -173,6 +195,7 @@ export default function Layout({ title, description, children }) {
                 ))}
               </List>
             </Drawer>
+
             <Box sx={isDesktop ? classes.visible : classes.hidden}>
               <form onSubmit={submitHandler}>
                 <Box sx={classes.searchForm}>
@@ -193,13 +216,13 @@ export default function Layout({ title, description, children }) {
               </form>
               </Box>
             <Box>
-            <Switch
+              <Switch
                 checked={darkMode}
                 onChange={darkModeChangeHandler}
               ></Switch>
               <NextLink href="/cart" passHref>
-              <Link>
-              <Typography component="span">
+                <Link>
+                  <Typography component="span">
                     {cart.cartItems.length > 0 ? (
                       <Badge
                         color="secondary"
@@ -256,7 +279,7 @@ export default function Layout({ title, description, children }) {
                 </>
               ) : (
                 <NextLink href="/login" passHref>
-                <Link>
+                  <Link>
                     <Typography component="span">Login</Typography>
                   </Link>
                 </NextLink>
